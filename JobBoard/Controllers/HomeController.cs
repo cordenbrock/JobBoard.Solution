9e6eb04395c5b745bdfef0f4d684c07cfb00e24f
Austin Schrader <austin.jschrader@gmail.com>
2020-12-15 10:42:20 -0800

    Add root controller

---
+++ JobBoard/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using JobBoard.Models;
+using System.Collections.Generic;
+
+namespace JobBoard.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      List<Job> allJobs = Job.GetAll();
+      return View(allJobs);
+    }
+  }
+}
